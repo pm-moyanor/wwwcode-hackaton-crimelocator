ff57e76e791f4f449473ff5e79ef0aa0dd9b6d16
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-10-19 13:33:30 -0400

    move colors data to own file

---
+++ crime-locator/colors.js
@@ -0,0 +1,60 @@
+const totalCountColors = [
+    { range: [1, 10], color: "rgba(255, 222, 0, 0.8)", width: 45, height: 45 },
+    { range: [10, 30], color: "rgba(255, 143, 50, 0.8)", width: 55, height: 55 },
+    { range: [30, 60], color: "rgba(102, 102, 0, 0.8)", width: 65, height: 65 },
+    { range: [60, 100], color: "rgba(153, 102, 0, 0.8)", width: 75, height: 75 },
+    { range: [100, 200], color: "rgba(204, 51, 0, 0.8)", width: 85, height: 85 },
+    {
+      range: [200, Infinity],
+      color: "rgba(255, 0, 51, 0.8)",
+      width: 65,
+      height: 65,
+    },
+  ];
+  
+  const categoryColors = [
+    {
+      crimeCategory: "Theft",
+      color: "rgba(0, 255, 0, 0.8)",
+      width: 25,
+      height: 25,
+    },
+    {
+      crimeCategory: "Burglary",
+      color: "rgba(51, 153, 0, 0.8)",
+      width: 35,
+      height: 35,
+    },
+    {
+      crimeCategory: "Assault",
+      color: "rgba(102, 102, 0, 0.8)",
+      width: 45,
+      height: 45,
+    },
+    {
+      crimeCategory: "Robbery",
+      color: "rgba(153, 102, 0, 0.8)",
+      width: 50,
+      height: 50,
+    },
+    {
+      crimeCategory: "Murder",
+      color: "rgba(204, 51, 0, 0.8)",
+      width: 55,
+      height: 55,
+    },
+    {
+      crimeCategory: "Rape",
+      color: "rgba(255, 51, 0, 0.8)",
+      width: 65,
+      height: 65,
+    },
+    {
+      crimeCategory: "Drug",
+      color: "rgba(255, 0, 51, 0.8)",
+      width: 75,
+      height: 75,
+    },
+  ];
+
+  export { totalCountColors, categoryColors };
--- crime-locator/src/CrimeMap.jsx
+++ crime-locator/src/CrimeMap.jsx
@@ -2,19 +2,14 @@ import React, { useRef, useEffect, useState } from "react";
 import axios from "axios";
 import mapboxgl from "mapbox-gl";
 import "./CrimeMap.css";
+import { totalCountColors, categoryColors } from "../colors"; // Import both arrays
 
+
+
+console.log(totalCountColors)
 mapboxgl.accessToken =
   "pk.eyJ1IjoicG1tb3lhbm9yIiwiYSI6ImNsbnVrZzN5bjBkOHkybHFqYXNzb3IxcjUifQ.99b7wHbQY6pShWHiVCNZnA";
-const zip = [
-  "85001",
-  "85003",
-  "85010",
-  "85012",
-  "85002",
-  "85004",
-  "85006",
-  "85013",
-];
+
 const crimeTypes = [
   "Theft",
   "Burglary",
@@ -37,70 +32,7 @@ const crimeTypes = [
 //header,footer,inputs
 //
 
-//dummy - adds a "totalcount value to the items to render different sizes and colors"
-const zipCodes = zip.map((location) => ({
-  location,
-  total_count: Math.floor(Math.random() * 250) + 1,
-}));
 
-const totalCountColors = [
-  { range: [1, 10], color: "rgba(255, 222, 0, 0.8)", width: 25, height: 25 },
-  { range: [10, 30], color: "rgba(255, 143, 50, 0.8)", width: 35, height: 35 },
-  { range: [30, 60], color: "rgba(102, 102, 0, 0.8)", width: 45, height: 45 },
-  { range: [60, 100], color: "rgba(153, 102, 0, 0.8)", width: 50, height: 50 },
-  { range: [100, 200], color: "rgba(204, 51, 0, 0.8)", width: 55, height: 55 },
-  {
-    range: [200, Infinity],
-    color: "rgba(255, 0, 51, 0.8)",
-    width: 65,
-    height: 65,
-  },
-];
-
-const categoryColors = [
-  {
-    crimeCategory: "Theft",
-    color: "rgba(0, 255, 0, 0.8)",
-    width: 25,
-    height: 25,
-  },
-  {
-    crimeCategory: "Burglary",
-    color: "rgba(51, 153, 0, 0.8)",
-    width: 35,
-    height: 35,
-  },
-  {
-    crimeCategory: "Assault",
-    color: "rgba(102, 102, 0, 0.8)",
-    width: 45,
-    height: 45,
-  },
-  {
-    crimeCategory: "Robbery",
-    color: "rgba(153, 102, 0, 0.8)",
-    width: 50,
-    height: 50,
-  },
-  {
-    crimeCategory: "Murder",
-    color: "rgba(204, 51, 0, 0.8)",
-    width: 55,
-    height: 55,
-  },
-  {
-    crimeCategory: "Rape",
-    color: "rgba(255, 51, 0, 0.8)",
-    width: 65,
-    height: 65,
-  },
-  {
-    crimeCategory: "Drug",
-    color: "rgba(255, 0, 51, 0.8)",
-    width: 75,
-    height: 75,
-  },
-];
 
 function getColorAndSize(total_count) {
   const matchingColorAndSize = totalCountColors.find((option) => {
@@ -120,6 +52,7 @@ function getColorAndSize(total_count) {
 const CrimeMap = ({ submittedValue }) => {
   const mapContainerRef = useRef(null); //save map container
   const [map, setMap] = useState(null);
+  const [totalCount, setTotalCount] = useState("");
 
   function formatDateForURL(startDate, endDate) {
     if (!startDate || !endDate) {
@@ -138,7 +71,7 @@ const CrimeMap = ({ submittedValue }) => {
   }
 
   const submittedValue1 = {
-    zipcode: "85003",
+    zipcode: "85307",
     dates: {
       startDate: "2020-10-03",
       endDate: "2023-10-04",
@@ -150,26 +83,29 @@ const CrimeMap = ({ submittedValue }) => {
   let baseURL = "";
   const formattedDate = formatDateForURL(startDate, endDate);
 
-  if (formattedDate) {
-    baseURL = `http://localhost:9090/crimeByZipcode?zipcode=${zipcode}&&category=theft&&start_date=${formattedDate.startDate}&&end_date=${formattedDate.endDate}`;
-    console.log(baseURL);
-  } else {
-    console.log("Empty date", startDate, endDate);
-  }
-
-  const getData = async (baseURL) => {
-    const options = {
-      method: "GET",
-      url: baseURL,
-    };
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
+  useEffect(() => {
+    if (formattedDate) {
+      baseURL = `http://localhost:9090/crimeByZipcode?zipcode=${zipcode}&&category=theft&&start_date=${formattedDate.startDate}&&end_date=${formattedDate.endDate}`;
+      console.log(baseURL);
+    } else {
+      console.log("Empty date", startDate, endDate);
     }
-  };
-  getData(baseURL);
+
+    const getData = async (baseURL) => {
+      const options = {
+        method: "GET",
+        url: baseURL,
+      };
+      try {
+        const response = await axios.request(options);
+        console.log(response.data.count);
+        setTotalCount(response.data.count);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData(baseURL);
+  }, [submittedValue]);
 
   useEffect(() => {
     const newMap = new mapboxgl.Map({
@@ -183,51 +119,46 @@ const CrimeMap = ({ submittedValue }) => {
       // Save the map instance in the state
       setMap(newMap);
 
-      const createMarkers = (data, colorOptions) => {
-        data.forEach((item) => {
-          //get coordinates for every zipcode provided
-          fetch(
-            `https://api.mapbox.com/geocoding/v5/mapbox.places/${item.location}.json?access_token=${mapboxgl.accessToken}`
-          )
-            .then((response) => response.json())
-            .then((geocodingData) => {
-              if (geocodingData.features && geocodingData.features.length > 0) {
-                const [longitude, latitude] = geocodingData.features[0].center;
+      const createMarker = (zipcode, totalCount) => {
+        fetch(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${zipcode}.json?access_token=${mapboxgl.accessToken}`
+        )
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((geocodingData) => {
+            if (geocodingData.features && geocodingData.features.length > 0) {
+              const [longitude, latitude] = geocodingData.features[0].center;
 
-                const customMarkerElement = document.createElement("div");
-                customMarkerElement.className = "custom-marker";
-                customMarkerElement.innerHTML = `<div class="marker-content">${
-                  item.total_count || ""
-                }</div>`;
+              const customMarkerElement = document.createElement("div");
+              customMarkerElement.className = "custom-marker";
+              customMarkerElement.innerHTML = `<div class="marker-content">${
+                totalCount || ""
+              }</div>`;
 
-                const matchingColorAndSize = getColorAndSize(
-                  item.total_count,
-                  colorOptions
-                );
-                const matchingColor = matchingColorAndSize.color;
-                const matchingSize = matchingColorAndSize;
+              const matchingColorAndSize = getColorAndSize(totalCount);
+              const matchingColor = matchingColorAndSize.color;
+              const matchingSize = matchingColorAndSize;
 
-                customMarkerElement.style.backgroundColor = matchingColor;
-                customMarkerElement.style.width = matchingSize.width + "px";
-                customMarkerElement.style.height = matchingSize.height + "px";
+              customMarkerElement.style.backgroundColor = matchingColor;
+              customMarkerElement.style.width = matchingSize.width + "px";
+              customMarkerElement.style.height = matchingSize.height + "px";
 
-                // Add a marker for each item
-                new mapboxgl.Marker({ element: customMarkerElement })
-                  .setLngLat([longitude, latitude])
-                  .addTo(newMap);
-              } else {
-                alert("No results found for the predefined location.");
-              }
-            })
-            .catch((error) => {
-              console.error("Geocoding error:", error);
-            });
-        });
+              new mapboxgl.Marker({ element: customMarkerElement })
+                .setLngLat([longitude, latitude])
+                .addTo(newMap);
+            } else {
+              alert("No results found for the predefined location.");
+            }
+          })
+          .catch((error) => {
+            console.error("Geocoding error:", error);
+          });
       };
-
-      // Create and add markers for zipCodes and crime categories
-      createMarkers(zipCodes, totalCountColors);
-      //createMarkers(crimeTypes, categoryColors);
+      createMarker(zipcode, totalCount, totalCountColors);
 
       const phoenixBounds = [
         [-112.3411, 33.3062], // Southwest coordinates
@@ -235,7 +166,6 @@ const CrimeMap = ({ submittedValue }) => {
       ];
 
       newMap.setMaxBounds(phoenixBounds);
-
       newMap.addControl(new mapboxgl.NavigationControl(), "top-left");
     });
 
