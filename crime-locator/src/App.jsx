69e2fd3a58941a1b775613158dd11024f7bf95f5
Shirley R <srm.shirley@gmail.com>
2023-10-19 17:09:21 -0500

    Add Header

--- crime-locator/src/App.jsx
+++ crime-locator/src/App.jsx
@@ -3,17 +3,20 @@ import CrimeMap from "./CrimeMap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import SearchForm from "./SearchForm";
 import { useState } from "react";
-
+import Header from "./components/Header";
 
 function App() {
   const [submittedValue, setSubmittedValue] = useState("");
 
 
   return (
-    <div >
-
-      <SearchForm submittedValue={submittedValue} setSubmittedValue={setSubmittedValue}/>
-      <CrimeMap submittedValue={submittedValue}/>
+    <div>
+      <Header />
+      <SearchForm
+        submittedValue={submittedValue}
+        setSubmittedValue={setSubmittedValue}
+      />
+      <CrimeMap submittedValue={submittedValue} />
     </div>
   );
 }
---
+++ crime-locator/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return <div className="bg-indigo-500">Crime Locator</div>;
+};
+
+export default Header;
